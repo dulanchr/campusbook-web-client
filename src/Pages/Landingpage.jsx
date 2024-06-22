cd20232011d952bb08982ded3f69cd8bc4dc7bbd
dula-dc <dulan.chr@gmail.com>
2024-06-22 13:34:32 +0530

    Deploy test 4

--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -2,7 +2,9 @@ export default function LandingPage() {
   return (
     <div className="bg-hero-pattern h-screen flex flex-col justify-center items-center">
       <p class="text-2xl-visby font-normal-visby">What's your A/L Stream?</p>
-      <p class="text-sm-visby max-w-lg text-center ">this sdsssis changes</p>
+      <p class="text-sm-visby max-w-lg text-center ">
+        this sdSomthing changes s sssssis changes
+      </p>
     </div>
   );
 }
