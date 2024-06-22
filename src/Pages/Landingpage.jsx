fb16708c6f2d7ff4e054a9b37a71da6c0679c8e8
dula-dc <dulan.chr@gmail.com>
2024-06-22 12:57:46 +0530

    deploy test

--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -2,13 +2,7 @@ export default function LandingPage() {
   return (
     <div className="bg-hero-pattern h-screen flex flex-col justify-center items-center">
       <p class="text-2xl-visby font-normal-visby">What's your A/L Stream?</p>
-      <p class="text-sm-visby max-w-lg text-center ">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore 
-      </p>
+      <p class="text-sm-visby max-w-lg text-center ">this is changes</p>
     </div>
   );
 }
